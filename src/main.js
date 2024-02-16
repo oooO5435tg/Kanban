86856936f22e0b231122ff5a23b9acbb8851ba53
oooO5435tg <morlean@inbox.ru>
2024-02-16 21:27:58 +0700

    колонки, добавление, удаление и перемещение задачи

--- src/main.js
+++ src/main.js
@@ -1,3 +1,62 @@
 new Vue({
     el: '#app',
+    data(){
+        return {
+            newTask: {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date().toLocaleString(),
+                lastChange: null,
+            },
+            plannedTasks: [],
+            progressTasks: [],
+            testingTasks: [],
+            completedTasks: [],
+        }
+    },
+    methods:{
+        addTask() {
+            if (!this.newTask.title) {
+                alert('Необходимо указать заголовок задачи');
+                return;
+            }
+            if (!this.newTask.description) {
+                alert('Необходимо указать описание задачи');
+                return;
+            }
+            if (!this.newTask.deadline) {
+                alert('Необходимо указать дэдлайн (срок сдачи задачи)');
+                return;
+            }
+            if (new Date(this.newTask.deadline) <= new Date(new Date().setDate(new Date().getDate()))) {
+                alert('Недействительная дата дэдлайна (минимум должен быть - завтра)');
+                return;
+            }
+            this.plannedTasks.push({...this.newTask});
+            this.newTask = {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date().toLocaleString(),
+                lastChange: null
+            }
+        },
+        removeTask(taskIndex) {
+            this.plannedTasks.splice(taskIndex, 1);
+        },
+        moveToInProgress(taskIndex) {
+            const taskToMove = this.plannedTasks.splice(taskIndex, 1)[0];
+            this.progressTasks.push(taskToMove);
+        },
+        moveToTesting(taskIndex) {
+            const taskToMove = this.progressTasks.splice(taskIndex, 1)[0];
+            this.testingTasks.push(taskToMove);
+        },
+        moveToCompleted(taskIndex) {
+            const taskToMove = this.testingTasks.splice(taskIndex, 1)[0];
+            taskToMove.isOverdue = new Date(taskToMove.deadline) < new Date();
+            this.completedTasks.push(taskToMove);
+        }
+    }
 })
